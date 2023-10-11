bac506e95c1350cc425c7bdd9dda042f5b7142db
Aleksandr K <alkaz05@mail.ru>
2023-10-11 12:43:09 +0300

    заготовка есть массив, есть вывод массива, но сортировка пока не работает

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] arr = {17, 14, 17, 15, 21, 31, 12, 16, 22};
+        System.out.println(Arrays.toString(arr));
+        bubbleSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void bubbleSort(int[] arr)
+    {
+
     }
 }

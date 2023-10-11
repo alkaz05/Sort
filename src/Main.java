3a469440cc468a2326096be86c92c973a0eeb6fe
Aleksandr K <alkaz05@mail.ru>
2023-10-11 12:56:54 +0300

    1 версия пузырька

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,15 @@ public class Main {
 
     public static void bubbleSort(int[] arr)
     {
-
+        for (int last= arr.length-1; last>0; last-- )        {
+            for (int k=0; k<last; k++)            {
+                if( arr[k] > arr[k+1])
+                {
+                    int tmp = arr[k];
+                    arr[k] = arr[k+1];
+                    arr[k+1] = tmp;
+                }
+            }
+        }
     }
 }
